86cce1df0d9bde6c544a512508cce3893ab5d87a
DESKTOP-AAOBN87\David <davidphilbin1@gmail.com>
2017-08-09 14:48:25 +0100

    Added Clickable Start Game, End Game, Made 2 C++ classes for Tank Controller and Tank, Reparented appropriate blueprints to these classes.

---
+++ 06_Tank_Battle/TankBattle/Source/TankBattle/TankPlayerController.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+
+
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+
+	auto ControlledTank = GetControlledTank();
+
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player Controller not possessing a tank!"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player Controller possessing a tank %s"), *(ControlledTank->GetName()));
+	}
+}
